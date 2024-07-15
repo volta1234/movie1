f9850a134814e610acef9113e884d4852a0d78e0
Volta1234 <calvince.ogwel@gmail.com>
2024-07-15 15:20:49 +0300

    first commit

---
+++ src/app/pages/movie/movie.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movie',
+  standalone: true,
+  imports: [],
+  templateUrl: './movie.component.html',
+  styleUrl: './movie.component.css'
+})
+export class MovieComponent {
+
+}
