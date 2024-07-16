c0b40fe0fc40f931bdbec04eb88b8d30fb9ee96e
Volta1234 <calvince.ogwel@gmail.com>
2024-07-16 11:05:38 +0300

    created and tested all the component routings

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { MovieComponent } from './pages/movie/movie.component';
+import { LoginComponent } from './pages/login/login.component';
 
-export const routes: Routes = [];
+export const routeConfig: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+        title: 'home page'
+    },
+    {
+        path: 'login',
+        component: LoginComponent,
+        title: 'login page'
+    },
+    {
+        path: 'movie',
+        component: MovieComponent,
+        title: 'movie page'
+    },
+    {
+        path: 'signup',
+        component: SignupComponent,
+        title: 'signup page'
+    }
+];
